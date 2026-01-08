3674aafcaa3ecad4003a623c0698cfaa3027239a
niyoxx <114039528+niyoxx@users.noreply.github.com>
2026-01-08 16:41:32 -0500

    init commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/niyoportfolio",
+  base: "/niyoportfolio/",
   server: {
     port: 3000,
   },
